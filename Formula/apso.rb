cfb1dcdb65441aabc2a66484d20eeed2e662d91f
cultron <mscullerton@gmail.com>
2026-05-26 09:50:30 -0500

    Update apso to 0.8.5 with real SHA256 checksums

--- Formula/apso.rb
+++ Formula/apso.rb
@@ -7,17 +7,17 @@ class Apso < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/apsoai/cli/releases/download/v#{version}/apso-v#{version}-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "febdb39617c97b2759f190d00cd1358086d98aa7fdf5b3ae54534f1847e5243e"
     elsif Hardware::CPU.intel?
       url "https://github.com/apsoai/cli/releases/download/v#{version}/apso-v#{version}-darwin-x64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "7ac90ef6b0c78ea9a524559e9ca466a0c0b8a5d8de08167183a1cb3dca049a3a"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/apsoai/cli/releases/download/v#{version}/apso-v#{version}-linux-x64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "f4cc3483f2708d9bea666e1966956552b1d702d34feb37006abfc965a267e33c"
     end
   end
 
